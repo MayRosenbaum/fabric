0d68fde7375daf2134788c5cdaa88237904f1469
May.Buzaglo <May.Buzaglo@ibm.com>
2026-05-13 12:18:57 +0300

    no op sig filter Signed-off-by: May Rosenbaum <mayro1595@gmail.com>
    Signed-off-by: May.Buzaglo <May.Buzaglo@ibm.com>

--- orderer/common/msgprocessor/sigfilter.go
+++ orderer/common/msgprocessor/sigfilter.go
@@ -7,14 +7,9 @@ SPDX-License-Identifier: Apache-2.0
 package msgprocessor
 
 import (
-	"fmt"
-
 	cb "github.com/hyperledger/fabric-protos-go-apiv2/common"
-	"github.com/hyperledger/fabric-protos-go-apiv2/orderer"
 	"github.com/hyperledger/fabric/common/channelconfig"
 	"github.com/hyperledger/fabric/common/policies"
-	"github.com/hyperledger/fabric/protoutil"
-	"github.com/pkg/errors"
 )
 
 // SigFilterSupport provides the resources required for the signature filter
@@ -46,35 +41,40 @@ func NewSigFilter(normalPolicyName, maintenancePolicyName string, support SigFil
 	}
 }
 
-// Apply applies the policy given, resulting in Reject or Forward, never Accept
+//// Apply applies the policy given, resulting in Reject or Forward, never Accept
+//func (sf *SigFilter) Apply(message *cb.Envelope) error {
+//	ordererConf, ok := sf.support.OrdererConfig()
+//	if !ok {
+//		logger.Panic("Programming error: orderer config not found")
+//	}
+//
+//	signedData, err := protoutil.EnvelopeAsSignedData(message)
+//	if err != nil {
+//		return fmt.Errorf("could not convert message to signedData: %s", err)
+//	}
+//
+//	// In maintenance mode, we typically require the signature of /Channel/Orderer/Writers.
+//	// This will filter out configuration changes that are not related to consensus-type migration
+//	// (e.g on /Channel/Application), and will block Deliver requests from peers (which are normally /Channel/Readers).
+//	policyName := sf.normalPolicyName
+//	if ordererConf.ConsensusState() == orderer.ConsensusType_STATE_MAINTENANCE {
+//		policyName = sf.maintenancePolicyName
+//	}
+//
+//	policy, ok := sf.support.PolicyManager().GetPolicy(policyName)
+//	if !ok {
+//		return fmt.Errorf("could not find policy %s", policyName)
+//	}
+//
+//	err = policy.EvaluateSignedData(signedData)
+//	if err != nil {
+//		logger.Warnw("SigFilter evaluation failed", "error", err.Error(), "ConsensusState", ordererConf.ConsensusState(), "policyName", policyName, "signingIdentity", protoutil.LogMessageForSerializedIdentities(signedData))
+//		return errors.Wrap(errors.WithStack(ErrPermissionDenied), err.Error())
+//	}
+//	return nil
+//}
+
+// Apply resulting in Accept every message, used for fabric performance
 func (sf *SigFilter) Apply(message *cb.Envelope) error {
-	ordererConf, ok := sf.support.OrdererConfig()
-	if !ok {
-		logger.Panic("Programming error: orderer config not found")
-	}
-
-	signedData, err := protoutil.EnvelopeAsSignedData(message)
-	if err != nil {
-		return fmt.Errorf("could not convert message to signedData: %s", err)
-	}
-
-	// In maintenance mode, we typically require the signature of /Channel/Orderer/Writers.
-	// This will filter out configuration changes that are not related to consensus-type migration
-	// (e.g on /Channel/Application), and will block Deliver requests from peers (which are normally /Channel/Readers).
-	policyName := sf.normalPolicyName
-	if ordererConf.ConsensusState() == orderer.ConsensusType_STATE_MAINTENANCE {
-		policyName = sf.maintenancePolicyName
-	}
-
-	policy, ok := sf.support.PolicyManager().GetPolicy(policyName)
-	if !ok {
-		return fmt.Errorf("could not find policy %s", policyName)
-	}
-
-	err = policy.EvaluateSignedData(signedData)
-	if err != nil {
-		logger.Warnw("SigFilter evaluation failed", "error", err.Error(), "ConsensusState", ordererConf.ConsensusState(), "policyName", policyName, "signingIdentity", protoutil.LogMessageForSerializedIdentities(signedData))
-		return errors.Wrap(errors.WithStack(ErrPermissionDenied), err.Error())
-	}
 	return nil
 }
